d909382a50fa59a36ebe0ad509bd288bf9eec858
xieziwei <1374884318@qq.com>
2023-05-14 02:14:32 +0800

    gtest

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "common/common.h"
 
 int main() {
---
+++ test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ziwei on 2023/5/14.
+//
+
+#include "gtest/gtest.h"
+
+TEST(AlohaTest, MainTest) {
+    int a = 1;
+    EXPECT_EQ(a, 1);
+}
