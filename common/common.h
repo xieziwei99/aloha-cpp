b66659018e67b5b34c1106ca5a0b6779d1dc9bb4
xieziwei <1374884318@qq.com>
2023-05-14 18:27:06 +0800

    Max

--- common/common.h
+++ common/common.h
@@ -13,5 +13,45 @@ void print(const T &t) {
     std::cout << t << std::endl;
 }
 
+template<typename T, typename Value, typename Filter>
+T Max(T begin, T end, const Value &value, const Filter &filter) {
+    if (begin == end) {
+        return end;
+    }
+    while (!filter(*begin)) {
+        begin++;
+    }
+    auto max_value = value(*begin);
+    T max_iter = begin;
+    for (T it = begin; it < end; it++) {
+        if (!filter(*it)) {
+            continue;
+        }
+        auto cur = value(*it);
+        if (cur > max_value) {
+            max_value = cur;
+            max_iter = it;
+        }
+    }
+    return max_iter;
+}
+
+template<typename T, typename Value>
+T Max(T begin, T end, const Value &value) {
+    auto filter = [](const auto &a) { return true; };
+    return Max(begin, end, value, filter);
+}
+
+template<typename T, typename Filter>
+T MaxAfterFilter(T begin, T end, const Filter &filter) {
+    auto value = [](const auto &a) { return a; };
+    return Max(begin, end, value, filter);
+}
+
+template<typename T>
+T Max(T begin, T end) {
+    auto value = [](const auto &a) { return a; };
+    return Max(begin, end, value);
+}
 
 #endif //ALOHA_COMMON_H
--- common/utils_test.cpp
+++ common/utils_test.cpp
@@ -13,3 +13,19 @@ TEST(UtilsTest, BinarySearchTest) {
     it = BinarySearch(arr, 5);
     EXPECT_EQ(it, arr.cbegin() + 3);
 }
+
+TEST(UtilsTest, MaxTest) {
+    std::vector<int> arr{4, 2, 5, 2, 4, 2, 5};
+    auto it = Max(arr.begin(), arr.end());
+    EXPECT_EQ(*it, 5);
+    EXPECT_EQ(it, arr.begin() + 2);
+
+    auto Value = [](const auto &a) { return std::abs(a - 4); };
+    auto Filter = [](const auto &a) { return a <= 4; };
+    it = Max(arr.begin(), arr.end(), Value);
+    EXPECT_EQ(*it, 2);
+    EXPECT_EQ(it, arr.begin() + 1);
+    it = MaxAfterFilter(arr.begin(), arr.end(), Filter);
+    EXPECT_EQ(*it, 4);
+    EXPECT_EQ(it, arr.begin());
+}
